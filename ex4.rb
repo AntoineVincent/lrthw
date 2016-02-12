e9d363e5c9fd454940318ff5853de41fb5d7c97f
Antoine et Lisa <anton51200@laposte.net>
2016-02-12 17:24:19 +0100

    up ex4 and create ex5

--- ex4.rb
+++ ex4.rb
@@ -14,3 +14,7 @@ puts "There will be #{cars_not_driven} empty cars today."
 puts "We can transport #{carpool_capacity} people today."
 puts "We have #{passengers} to carpool today."
 puts "We need to put about #{average_passengers_per_car} in each car."
+
+# Study drills
+# L'erreur devait être du au fait qu'il n'avait
+# pas défini la fonction carpool capacity donc la ligne 7
