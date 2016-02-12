77c9731980f6d99a0aece979cece1f3557ad0fd9
Antoine et Lisa <anton51200@laposte.net>
2016-02-12 15:59:17 +0100

    try study drills in ex1

--- ex1.rb
+++ ex1.rb
@@ -5,3 +5,6 @@ puts "This is fun."
 puts "Yay! printing."
 puts "I'd much rather you 'not' ."
 puts 'I "said" do not touch this.'
+
+#puts " test the octothorpe"
+# the octothorpe disable the line in the script
