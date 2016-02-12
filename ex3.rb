aea7350b43fecb8f58b2fdc5f2ed7269c8238778
Antoine et Lisa <anton51200@laposte.net>
2016-02-12 15:53:15 +0100

    Fini les exercices 1 à 3

---
+++ ex3.rb
@@ -0,0 +1,24 @@
+puts "I will now count my chickens:"
+
+puts "Hens #{25 + 30 / 6}"
+puts "Rooster #{100 - 25 * 3 % 4}"
+# puts is saying what to run, and the mathematical operation is automaticly done with #{}
+puts "Now I will count the eggs :"
+
+puts 3 + 2.0 + 1 - 5 + 4.0 % 2 - 1 / 4 + 6.0
+# test floating point on basic operations. The result is the same with the floating point
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 > -2}"
+puts "Is it less or equal? #{5 <= -2}"
+# These function say us what is false or true in these equation
